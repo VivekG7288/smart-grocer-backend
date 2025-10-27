23895c4d90dcfbe47e944f45e33a7d5c00dc332d
vivekweberon <vivek@weberon.net>
2025-10-27 19:27:26 +0530

    Fixing deployemnt issues

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectDB from "./db/connection.js";
+import connectDB from "./src/config/db.js";
 
 dotenv.config();
 const app = express();
